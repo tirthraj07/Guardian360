b0747a783c57c8bd91984c19724e47b4d24940a8
Amey Kulkarni <148298416+ameyk2004@users.noreply.github.com>
2025-02-05 13:32:57 +0530

    Changed incident Reporting Route

--- incident_reporting_service/app/repository/incident_subtypes_repository.py
+++ incident_reporting_service/app/repository/incident_subtypes_repository.py
@@ -5,3 +5,9 @@ class IncidentSubTypesRepository:
     def get_all_by_type(typeID):
         response = supabase.table("incident_sub_types").select("*").eq("typeID", typeID).execute()
         return response.data if response.data else []
+    
+    @staticmethod
+    def get_sub_type_by_id(subtypeID):
+        response = supabase.table("incident_sub_types").select("sub_type").eq("subtypeID", subtypeID).execute()
+        print(response.data)
+        return response.data if response.data else []
