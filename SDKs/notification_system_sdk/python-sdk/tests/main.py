f945f1209e56295c63923df11a224556795f7c94
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2025-02-07 22:49:45 +0530

    updated travel service

--- SDKs/notification_system_sdk/python-sdk/tests/main.py
+++ SDKs/notification_system_sdk/python-sdk/tests/main.py
@@ -3,7 +3,6 @@ from guardian360_notification_utils.client import Guardian360NotificationClient
 notification_client = Guardian360NotificationClient(
     base_url="http://143.110.183.53/notification-service",
     # base_url="http://localhost:9000",
-<<<<<<< Updated upstream
     debug_mode=True
 )
 
@@ -15,29 +14,15 @@ notification_client = Guardian360NotificationClient(
 #     inapp_message=None,
 #     push_message=None
 # )
-user_details = {
-    'first_name' : 'Amey',
-}
 
-source_location = "https://www.google.com/maps?q=18.4690213,73.8640944"
-destination_location = "https://www.google.com/maps?q=18.4690213,73.8640944"
-
-notification_client.send_travel_alert_notification(
-            user_id=36,
-            message= f"{user_details['first_name']} is traveling. You'll be notified again in 8 mins.",
-            email_message=f"""{user_details['first_name']} has started traveling. You can track their journey in real time via our app.<br><br>Stay updated with live location and notifications.<hr>Location Details : Click to view<br><br><a href =\"{source_location}\">Source Location</a><br><br><a href =\"{destination_location}\">Destination Location</a>"""
-=======
-    debug_mode=False
-)
 
 notification_client.send_sos_notification(
     user_id=36,
     message="This is the default message for any channel not otherwise specified.",
     email_message="This message is specific to email",
-    sms_message="This message is specific to sms",
+    sms_message="This message is specific to sms. hello amu",
     inapp_message=None,
     push_message=None
->>>>>>> Stashed changes
 )
 
 # notification_client.send_travel_alert_notification(
