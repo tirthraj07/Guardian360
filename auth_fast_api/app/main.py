b8a438825c6f53666bc48e5cec75020a966d0def
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2025-02-12 18:21:54 +0530

    removed auth_service_fastapi

--- auth_fast_api/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI, HTTPException, Depends
-from pydantic import BaseModel, EmailStr
-from app.services.login import router as login_router
-
-
-app = FastAPI()
-
-app.include_router(login_router)
